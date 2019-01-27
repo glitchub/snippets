d3faa2adf8fff365b5b98f695b20897b9751b056
glitchub <glitchub@users.noreply.github.com>
2019-01-27 13:39:38 -0800

    Comply with -Werror, reduce qs stack

--- bs.c
+++ bs.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Optimized bubble sort. Useful for small data sets or in embedded context
+// where stack size is limited. Otherwise use quick sort!
 
-// optimized bubble sort
-int bs(int *a, int n)
+// Given array of n ints, sort the array in place
+void bs(int *a, int n)
 {
     int i, l;
-    while (l=n) for (i=n=0; i<l-1; i++) if (a[i] > a[i+1]) a[i] ^= (a[i+1] ^= (a[i] ^= a[i+1])), n=i+1;
-}    
+    while (n) for (l=n, n=0, i=1; i<l; i++) if (a[i] < a[i-1]) a[i]^=a[i-1], a[i-1]^=a[i], a[i]^=a[i-1], n=i;
+}
+
+#if 1
+// POC, accept integers on command line and print sorted list, e.g.:
+//   ./bs $(for x in {1..10000}; do echo $RANDOM; done)
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc , char *argv[])
 {
     int vs=argc-1, v[vs], x;
     for (x=0; x < vs; x++) v[x]=atoi(argv[x+1]);
     bs(v, vs);
-    for (x=0; x < vs; x++) printf("%d ",v[x]); printf("\n");
-} 
+    for (x=0; x < vs; x++) printf("%d\n",v[x]);
+    return 0;
+}
+#endif
--- qs.c
+++ qs.c
@@ -1,30 +1,39 @@
+// Quick sort, requires around log2(n)*6 words of free stack, where 'n' is the
+// number of elements to sort and 'word' is sizeof(void *). This generally
+// rules out embedded systems, in that case use bubble sort.
+
+// Given two pointers into a contiguous array of ints, recursively sort the
+// elements between the pointers.
+static void qs(int *first, int *last)
+{
+    if (first < last)
+    {
+        int *f=first, *l=last;
+        while (1)
+        {
+            while (*f < *first) f++;
+            while (*l > *first) l--;
+            if (f >= l) break;
+            // swap *f and *l
+            *l^=*f; *f^=*l; *l--^=*f++;
+        }
+        qs(first, l);
+        qs(l+1, last);
+    }
+}
+
+#if 1
+// POC, accept integers on command line and print sorted list, e.g.:
+//   ./qs $(hexdump -n20000 -e '1/2 "%d\n"' /dev/urandom)
 #include <stdio.h>
 #include <stdlib.h>
 
-static void qs(int *a, int lo, int hi) 
+int main(int argc , char *argv[])
 {
-    if (hi > lo)
-    {
-        int l=lo, h=hi, p=a[(hi+lo)/2];
-        while (1)
-        {
-            int t;
-            while (a[l] < p) l++;
-            while (a[h] > p) h--;
-            if (l >= h) break;
-            t=a[l]; a[l++]=a[h]; a[h--]=t;
-        }
-        qs(a, lo, h); 
-        qs(a, h+1, hi);  
-    }   
+    int vs=argc-1, v[vs], x;
+    for (x=0; x < vs; x++) v[x]=atoi(argv[x+1]);
+    qs(v, v+vs-1);
+    for (x=0; x < vs; x++) printf("%d\n",v[x]);
+    return 0;
 }
-
-int main(int argc, char *argv[]) 
-{
-   int x, size=argc-1, array[size];
-   for (x = 0; x < size; x++) array[x]=atoi(argv[x+1]);
-   qs(array, 0, size-1);
-   for (x = 0; x < size; x++) printf("%d ",array[x]); 
-   printf("\n");
-   return 0;
-}
+#endif
