96f7591e8cee0a2d62f9e5b9b3b5642099324757
glitchub <glitchub@users.noreply.github.com>
2022-12-18 16:32:20 -0800

    sync

--- csvlite.sh
+++ csvlite.sh
@@ -5,7 +5,7 @@ die() { echo "$@" >&2; exit 1; }
 usage() { die "\
 Usage:
 
-    csvlite [options] ['command' [...'command']] file.csv
+    $0 [options] file.csv ['command' [... 'command']]
 
 Perform sqlite3 commands against the specified csv file. The first line of the file is assumed to contain column
 names.
@@ -13,16 +13,19 @@ names.
 Options are:
 
     -o file  - write resulting csv to the specified file on exit
+    -r       - read-only, do not update database on exit
     -t name  - name of generated sql table, default is 'csv'
 
-If commands are specified then they are executed in the order given, and any error will cause the script to terminate
-with an error.
+If commands are specified then they are executed in the order given. Exit due to error will cause the script to
+abort.
 
-Otherwise an interactive shell is opened and abnormal exit is ignored.
+Otherwise an interactive shell is opened. Exit due to ^\ or other signal will cause script to abort.
 
-After the commands are executed or the shell is exited, 'file.csv' will be updated only if the database has changed.
-However if -o is given then the data is unconditionally written to the specified file instead. Use '-o /dev/null' to
-disable updates completely, '-o /dev/stdout' to write the result to the console, etc.
+After commands are processed or shell terminates, if -r was specified then the script exits.
+
+If -o was not given then 'file.csv' will be updated if the database was changed.
+
+If -o was given then the designated file will be unconditionally written.
 
 Note that csv is a pure-text format, therefore all columns are treated as TEXT, there are no indexes or keys, and NULL
 fields are converted to empty strings.
@@ -40,16 +43,17 @@ set -o noglob
 
 out=
 table=csv
-while getopts ":o:t:" o; do case $o in
+ro=0
+while getopts ":o:rt:" o; do case $o in
     o) out=$OPTARG ;;
+    r) ro=1 ;;
     t) table=$OPTARG ;;
     *) usage ;;
 esac; done
 shift $((OPTIND-1))
 
 (($#)) || usage
-commands=("${@:1:$#-1}")
-csvfile=${@:$#}
+csvfile=$1
 [[ -s $csvfile ]] || die "No such file $csvfile"
 
 db=$(mktemp -t csvlite.XXXXXXXX)
@@ -58,14 +62,18 @@ trap "rm -f $db" EXIT
 # read csv to database
 sqlite3=$(type -P sqlite3) || die "Need executable sqlite3"
 $sqlite3 -bail -csv $db ".import $csvfile $table" || die "Unable to import csv from $csvfile"
-[[ $out ]] || md5=$(md5sum $db)
+md5=$(md5sum $db)
 
-if [[ ${commands:-} ]]; then
-    # perform commands
-    $sqlite3 -bail $db "${commands[@]}" || die "sqlite3 command failed"
+if (($# > 1)); then
+    # perform commands, die on error
+    $sqlite3 -bail $db "${@:2}" || die "sqlite3 command failed"
 else
-    # interactive, ignore error result
+    # interactive, die on ^\, etc
     $sqlite3 $db -cmd '.prompt "csvlite> " "> "'
+    (($? >= 128)) && die "sqlite3 abnormal exit"
 fi
 
-[[ $out || $md5 != $(md5sum $db) ]] && $sqlite3 -csv $db/db ".headers on" "select * from $table" > ${out:-$file}
+# maybe update
+if ((!ro)) && [[ $out || $md5 != $(md5sum $db) ]]; then
+    $sqlite3 -csv $db ".headers on" "select * from $table" > ${out:-$csvfile}
+fi
