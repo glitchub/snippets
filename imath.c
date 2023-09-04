9661429c4628fbf86d4b5373be6f191c85cbb6b9
glitchub <glitchub@nowhere>
2023-09-04 16:29:01 -0700

    math

--- imath.c
+++ imath.c
@@ -1,51 +1,66 @@
 #include <stdlib.h>
+#include <stdint.h>
 
-// Return x to the n power (or 0 on overflow)
-unsigned long long ipow(unsigned int x, unsigned int n)
+// Return integer x to the nth power (or 0 on overflow)
+uint64_t ipow(uint64_t x, uint32_t n)
 {
-    unsigned long long xx = x, pow = 1;
-    if (!x) return 0;
+    uint64_t pow = 1;
+    if (!x || x >= (1L << 32)) return 0;
     if (!n) return 1;
     if (n == 1) return x;
     while(1)
     {
-        if (n & 1 && __builtin_mul_overflow(pow, xx, &pow)) return 0;
+        if (n & 1 && __builtin_mul_overflow(pow, x, &pow)) return 0;
         if (!(n >>= 1)) return pow;
-        if (__builtin_mul_overflow(xx, xx, &xx)) return 0;
+        if (__builtin_mul_overflow(x, x, &x)) return 0;
     }
 }
 
-
-// Return nth root of x (or 0 on error/overflow)
-unsigned int iroot(unsigned long long x, unsigned int n)
+// Return integer nth root of x (or 0 on error/overflow)
+uint32_t iroot(uint64_t x, uint32_t n)
 {
     if (!n || !x) return 0;
     if (n == x) return 1;
     if (n == 1 || x == 1) return x;
 
-    unsigned long long base = 1;
-    unsigned long long root = 2;
+    uint64_t base = 1, root = 2;
 
-    // find smallest power of 2 greater than x
+    // Start with largest power of 2 less than the nth root
     while(1)
     {
-        unsigned long long r = ipow(root, n);
+        uint64_t r = ipow(root, n);
         if (r == x) return root; // stumbled upon
         if (!r || r > x) break;
         base = r;
         root <<= 1;
     }
+    root >>= 1;
 
-    root >>= 1; // start with largest power of 2 less than x
-
-    for (unsigned int bit = root >> 1; bit && x > base; bit >>= 1)
+    for (uint32_t min = root >> 1; min && x > base; min >>= 1)
     {
-        unsigned long long r = ipow(root + bit, n);
-        if (!r) return 0; // shouldn't happen
-        if (r <= x)
+        uint64_t r = ipow(root + min, n);
+        if (r && r <= x)
         {
-            root += bit;
+            root += min;
             base = r;
+        }
+    }
+    return root;
+}
+
+// Return integer square root of x.
+uint32_t isqrt(uint64_t x)
+{
+    if (x <= 1) return x;
+    uint64_t root = 0;
+    for (uint64_t pow4 = 1L << 62; pow4; pow4 >>= 2)
+    {
+        uint64_t base = root + pow4;
+        root >>= 1;
+        if (x >= base)
+        {
+            x -= base;
+            root += pow4;
         }
     }
     return root;
@@ -58,12 +73,15 @@ unsigned int iroot(unsigned long long x, unsigned int n)
 int main(int argc, char *argv[])
 {
     // expect two numeric arguments
-    unsigned long long x = strtoull(argv[1],NULL,0);
-    unsigned int n = strtoul(argv[2],NULL,0);
+    uint64_t x = strtoull(argv[1],NULL,0);
+    uint32_t n = strtoul(argv[2],NULL,0);
 
-    unsigned int r = iroot(x, n);
+    uint32_t s = isqrt(x);
+    if (!s) printf("2√%llu error\n", x); else printf("2√%llu = %u, %u^2 = %llu\n", x, s, s, (uint64_t)s * s);
+
+    uint32_t r = iroot(x, n);
     if (!r) printf("%u√%llu error\n", n, x); else printf("%u√%llu = %u, %u^%u = %llu\n", n, x, r, r, n, ipow(r, n));
 
-    unsigned long long p = ipow(x, n);
+    uint64_t p = ipow(x, n);
     if (!p) printf("%llu^%u error\n", x, n); else printf("%llu^%u = %llu, %u√%llu = %u\n", x, n, p, n, p, iroot(p, n));
 }
