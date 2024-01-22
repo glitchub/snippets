5106b02fccab366c4701f75164f0a407676696c1
glitchub <glitchub@nowhere>
2024-01-22 13:25:59 -0800

    Add isbom

--- istext.sh
+++ istext.sh
@@ -5,10 +5,20 @@ istext() { ! LC_ALL=en_US.utf8 grep -q '[^[:print:][:space:]]' $1; }
 # Return true if specified file is plain ASCII text.
 isplain() { ! LC_ALL=C grep -q '[^[:print:][:space:]]' $1; }
 
-for f in /etc/passwd ./utf8.txt /bin/ls; do
-    if istext $f; then
-        if isplain $f; then
+# Return true if specified file starts with UTF-8 byte order mark.
+# https://en.wikipedia.org/wiki/Byte_order_mark
+hasbom() { awk '/^\xef\xbb\xbf/{exit 0}{exit 1}' $1; }
+
+# test with specified or default files
+[ $# = 0 ] && set /etc/passwd ./utf8.txt /bin/ls
+for f in "$@";do
+    if ! [ -f $f ]; then
+        echo "$f is not a file"
+    elif istext "$f"; then
+        if isplain "$f"; then
             echo "$f is plain text"
+        elif hasbom "$f"; then
+            echo "$f is UTF-8 text with a BOM"
         else
             echo "$f is UTF-8 text"
         fi
