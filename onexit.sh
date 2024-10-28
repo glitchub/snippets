fc87aececc522a386e0e84f64783f0fe9f503bda
glitchub <glitchub@nowhere>
2024-10-27 21:25:07 -0700

    onexit

--- onexit.sh
+++ onexit.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Stacked exit handler for bash. Commands defined with 'onexit' or 'onerror' are performed at exit,
-# in reverse order of definition. 'onerror' commands are only performed if exit status is non-zero.
-# NOTE: single quoted command strings are expanded during exit, unquoted or double quoted command
-# strings are expanded at the time of definition. Strings must not end with ';'!
-trap '__=$?; set +ue; eval $onexit; exit $__' EXIT
-onexit() { onexit="$*; ${onexit:-}"; }
-onerror() { onexit="((\$__)) && { $*; }; ${onexit:-}"; }
+# Stacked exit handler for bash. Commands defined with "onexit" or "onerror" are performed at exit,
+# in reverse order of definition. "onerror" commands are only performed if exit status is non-zero.
+# Any arbitrary command pipeline is allowed but must not end with ";". Normal eval quoting rules
+# apply.
+trap '__exs=$?;trap debug;trap return;trap err;set +ueET;eval $__oxc' EXIT
+onexit() { __oxc="$*;${__oxc:-}";}
+onerror() { onexit "((__exs))&&{ $*;}";}
 
 # The rest of this file is a test, run it with 'bash onexit.sh; echo $?'.
 
-set -ue
+set -ueE
+
+trap "echo ERROR on line $LINENO" err
 
 onexit echo First in, last out.
-onerror 'echo This only appears because the exit status is $__; echo And also this!'
-onexit 'echo Flag=$-; nosuchcommand; test $nosuchvar; echo Ignore the above!'
+onexit 'false; echo FALSE=$?'
+
+onerror 'echo This only appears because the exit status is non-zero; echo And also this!'
+onexit 'echo Flag=$-; nosuchcommand; test $nosuchvar; echo Ignore the errors above!'
 
 # create a temp directory
 dir=$(mktemp -td onexit.poc.XXXX) || exit 1
@@ -31,12 +35,14 @@ touch $dir/xyzzy || exit 1
 
 onexit echo Last in, first out
 
-ex=$((RANDOM % 5))
+(($#)) && ex=$1 || ex=$((RANDOM % 4))
+
+# shellcheck disable=SC2154
 case $ex in
-    0) printf "\n---- Normal exit\n\n"; exit 0 ;;
-    2) printf "\n---- Abort due to -e\n\n"; false ;;
-    3) printf "\n---- Abort due to -u\n\n"; echo $nosuchvar ;;
-    *) printf "\n---- Error exit $ex\n\n"; exit $ex ;;
+    0) printf "\n0 ---- Normal exit\n\n"; exit 0 ;;
+    1) printf "\n1 ---- Abort due to -e\n\n"; false ;;
+    2) printf "\n2 ---- Abort due to -u\n\n"; echo $nosuchvar ;;
+    *) printf "\n* ---- Error exit $ex\n\n"; exit $ex ;;
 esac
 echo "SHOULDN'T GET HERE"
 exit 99
