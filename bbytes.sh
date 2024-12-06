341031f83b37eda08be66fe10a59ef1ebc518b06
glitchub <glitchub@nowhere>
2024-12-05 16:22:01 -0800

    bbytes?

--- bbytes.sh
+++ bbytes.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-# Convert convert to fractional kibibytes, mebibytes, etc (powers of 2), ie 1048576 -> 2.00MiB
+# Print number as fractional kibibytes, mebibytes, etc (powers of 2), ie 1048576 -> "2.00 MiB"
 # If given, second arg if given is number of signficant digits after the decimal, default 2
 bbytes() {
-    [[ $1 =~ [^0-9] ]] && { echo $1; return; }
-    local n=$((10#$1)) s=${2:-2} f="%d%0s%s\n" p=0 d=$((2**60))
-    ((s)) && { f="%d.%0${s}d%s\n" p=$((2**s)); }
-    ((n < 1024)) && { printf $f $n 0 ""; return; }
-    for m in EiB PiB TiB GiB MiB KiB; do
+    [[ $1 =~ [^0-9] ]] && { echo "$1"; return; }
+    local n=$((10#$1)) s=${2:-2}
+    ((n < 1024)) && { echo "$n bytes"; return; }
+    local d=$((2**50))
+    for m in PiB TiB GiB MiB KiB; do
         ((n >= d)) && break
         ((d /= 1024))
     done
-    printf $f $((n/d)) $(((n % d)* p / d)) $m
+    # s is precision, 0 = no decimal
+    ((s)) && printf "%d.%0*d %s\n" "$((n/d))" "$s" "$(((n%d)*(10**s)/d))" "$m" || printf "%d %s\n" "$((n/d))" "$m";
 }
 
-# Print numeric arg, convert to fractional kilobytes, megabytes, etc (powers of 10), ie 1000000 -> 2.00MB
+# Print number as fractional kilobytes, megabytes, etc (powers of 10), ie 1000000 -> "2.00 MB"
 # If given, second arg if given is number of signficant digits after the decimal, default 2
 dbytes() {
-    [[ $1 =~ [^0-9] ]] && { echo $1; return; }
-    local n=$((10#$1)) s=${2:-2} f="%d%0s%s\n" p=0 d=$((10**18))
-    ((s)) && { f="%d.%0${s}d%s\n" p=$((2**s)); }
-    ((n < 1000)) && { printf $f $n 0 ""; return; }
-    for m in EB PB TB GB MB KB; do
+    [[ $1 =~ [^0-9] ]] && { echo "$1"; return; }
+    local n=$((10#$1)) s=${2:-2}
+    ((n < 1000)) && { echo "$n bytes"; return; }
+    local d=$((10**15))
+    for m in PB TB GB MB KB; do
         ((n >= d)) && break
         ((d /= 1000))
     done
-    printf $f $((n/d)) $(((n%d)*(10**s)/d)) $m
+    # s is precision, 0 = no decimal
+    ((s)) && printf "%d.%0*d %s\n" $((n/d)) $s $(((n%d)*(10**s)/d)) $m || printf "%d %s\n" $((n/d)) $m;
 }
 
 echo "bbytes: $(bbytes $1 $2)"
