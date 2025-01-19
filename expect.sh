d50d2a1e2f08d3c7b3d3607189658874d8f15bb6
glitchub <glitchub@nowhere>
2025-01-18 19:03:25 -0800

    More fixes

--- expect.sh
+++ expect.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-# Expect functionality in bash. Running this script executes example code at the bottom.
-# In practice, this entire block is copied to your script or sourced from a separate file.
-
 {
-    # This provides expect functionality, including "spawn", "send", and "expect". More
+    # This provides expect functionality in bash, including "spawn", "send", and "expect". More
     # information in the "public functions" section below.
 
     # Private symbols start with "__", avoid direct reference.
 
     __expect=$(type -pf expect 2>/dev/null) || { echo "Requires executable 'expect'" >&2; exit 1; }
 
-    # start expect coprocess
-    __started=0
-    __start() {
-        if ((!__started)); then
-            local cp exp; coproc cp {
-                exec {exp}<<<"log_user 0; set stty_init {-opost -echo}
-                proc enc {s} {binary encode base64 [encoding convertto utf-8 \$s]}
-                proc dec {s} {encoding convertfrom utf-8 [binary decode base64 \$s]}
-                while {[gets stdin line] >= 0} {eval \$line}"
-                exec $__expect -f /dev/fd/$exp
-            }
-            exec {__cpout}<&"${cp[0]}" {__cpin}>&"${cp[1]}"; __started=1
-        fi
-    }
-
     # __cpio [options] tcl, send tcl to coproc, echo non-null response. Die on coprocess failure.
     # -d: decode response, -t timeout: override 10
-    __debio=0
+    __debio=0 __started=0 __cpout="" __cpin=""
     __cpio() {
+        if ((!__started)); then
+            coproc {
+                exec $__expect -c 'log_user 0; set stty_init {-opost -echo}
+                                   proc enc {s} {binary encode base64 [encoding convertto utf-8 $s]}
+                                   proc dec {s} {encoding convertfrom utf-8 [binary decode base64 $s]}
+                                   while {[gets stdin line] >= 0} {if [catch {eval $line} err] {puts "1:$err"}}'
+            }
+            exec {__cpout}<&"${COPROC[0]}" {__cpin}>&"${COPROC[1]}"
+            __started=1
+        fi
         local OPTARG OPTIND to=10 out=cat s
         while getopts ":dt:" s; do case $s in d)out="__dec";; t)to=$OPTARG;; *);; esac; done
         # shellcheck disable=2048 # compressing whitespace
@@ -40,15 +32,15 @@
     }
 
     __enc () {
-        $__expect -c "fconfigure stdin -translation binary
+        $__expect -c 'fconfigure stdin -translation binary
                       puts -nonewline [binary encode base64 [encoding convertto utf-8 [read -nonewline stdin]]]
-                      flush stdout"
+                      flush stdout'
      }
 
     __dec () {
-        $__expect -c "fconfigure stdout -translation binary
+        $__expect -c 'fconfigure stdout -translation binary
                       puts -nonewline [encoding convertfrom utf-8 [binary decode base64 [read -nonewline stdin]]]
-                      flush stdout"
+                      flush stdout'
     }
 
     # "Public" functions, all return true if success and false if error. These are roughly analogous to
@@ -56,12 +48,12 @@
 
     # spawn program [args] - start specified program in the background
     spawn() {
-        __start; local prog; readarray -t prog< <(for a; do printf "%s" "$a" | __enc; echo; done)
-        __cpio "if [catch {if [spawn -noecho {*}[lmap arg {${prog[*]}} {dec \$arg}]] {puts 0} {puts 1}} err] {puts \"1:\$err\"}"
+        local prog; readarray -t prog< <(for a; do printf "%s" "$a" | __enc; echo; done)
+        __cpio "if [spawn -noecho {*}[lmap arg {${prog[*]}} {dec \$arg}]] {puts 0} {puts 1}"
     }
 
     # Print the current spawn id (the id of the most recently spawned program)
-    spawnid() { __cpio 'if [catch {puts "0:$spawn_id"} err] {puts 1:$err}'; }
+    spawnid() { __cpio 'puts "0:$spawn_id"'; }
 
     # close [options] - close the current spawnid, in theory this will cause the process to
     # exit. Then reap the child if possible. Options:
@@ -76,7 +68,7 @@
     #    -i spawnid : write to specified spawnid instead
     send() {
         local OPTARG OPTIND i="" s; while getopts ":i:" o; do case $o in i) i="-i $OPTARG";; *);; esac; done
-        s="${*:$OPTIND}"; __cpio "if [catch {send $i [dec $(__enc <<< ${s@E})]; puts 0} err] { puts \"1:\$err\" }"
+        s="${*:$OPTIND}"; __cpio "send $i [dec $(__enc <<< ${s@E})]; puts 0"
     }
 
     # expect [options] regex [...regex]
@@ -94,18 +86,19 @@
         while getopts ":gi:t:" s; do case $s in g)p="(?n)";; i)cmd=("-i [list $OPTARG]");; t)to=$OPTARG;; *);; esac; done
         shift $((OPTIND-1)); for s; do cmd+=("-regex [dec $(__enc <<< "$p$s")] {set result $((n++))}"); done
         [[ $to =~ ^[1-9][0-9]*$ ]] || to=10 # valid or 10
-        __cpio -t $((to+1)) "if [catch {array unset expect_out; set matches 0; expect -timeout $to
-        ${cmd[*]} timeout {set result timeout} eof {set result eof}; set matches [llength [lsearch
-        -all [array names expect_out] *string]]; puts \"0:\$result\" } err] {puts \"1:\$err\"}"
+        __cpio -t $((to+1)) "array unset expect_out; set matches 0;
+                             expect -timeout $to ${cmd[*]} timeout {set result timeout} eof {set result eof};
+                             set matches [llength [lsearch -all [array names expect_out] *string]];
+                             puts \"0:\$result\""
     }
 
     # match [n] - print nth match string from last expect, 0 for the match itself (default),
     # otherwise the regex submatch.
+    # shellcheck disable=2120 # arg is optional
     match() {
         local n=${1:-0}
-        __cpio -d "if {$n >= \$matches} {puts \"1:$n >= \$matches\" } elseif [catch {puts \"0:[enc
-        [string map {\"\r\n\" \"\n\" \"\n\r\" \"\n\" \"\r\" \"\n\"} \$expect_out($n,string)]]\" }
-        err] {puts \"1:\$err\"}"
+        __cpio -d "if {$n >= \$matches} {puts \"1:$n >= \$matches\"}
+                   {puts \"0:[enc [string map {\"\r\n\" \"\n\" \"\n\r\" \"\n\" \"\r\" \"\n\"} \$expect_out($n,string)]]\"}"
     }
 
     # Print the spawnid that produced the match or eof from the last expect.
@@ -116,8 +109,45 @@
     debsp() { __start; __cpio 'log_file -a -leaveopen stderr; puts 0'; }    # write spawned process i/o to stderr
 }
 
-# Example/test: telnet to http hosts in parallel, send them all GET requests and scrape
-# "Date:" from their response headers.
+die() { echo "$*" >&2; exit 1; }
+
+usage() {
+    cat <<EOT >&2
+Usage:
+
+    ${0##*/} [options]
+
+Telnet to the CPMs in a chassis and then repeatedly power cycle it, tracking the timing of the CPM boots.
+
+Options:
+
+    -a ip:port             - CPMA console
+    -b ip:port             - CPMB console
+    -g hostname            - gash server
+    -p ip[:port]/outlet    - Power controller telnet and outlet name, for use with pwrctrl
+    -t N                   - Set system type N (default 0)
+
+-a is required.
+
+If -b is not provided then just tests boot time of the single CPM.
+
+If the CPMs are in a regression test bed then provide -g, the server must be accessible via ssh.
+
+Otherwise provide -p, the specified controller must be supported by the pwrctrl utility.
+
+Type 0 systems boot through the UEFI chain to grub.
+Type 1 systems boot through the EFI shell.
+
+EOT
+    exit 1
+}
+
+# In practice you copy the block above to your script or source it from a seperate file.
+
+# POC: telnet to http hosts in parallel, send them all GET requests and scrape the "Date:" from
+# their response headers.
+
+set -ueE
 
 declare -A spawned # an array of [spawnid]=hostname
 
