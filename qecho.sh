a7c386c82a7342472073211acf87e646f64bed7d
glitchub <glitchub@nowhere>
2025-05-01 13:38:16 -0700

    escape embedded quotes

--- qecho.sh
+++ qecho.sh
@@ -1,8 +1,9 @@
 # same as bash echo, but single quote any args that contain whitespace
-qecho() { local a=() t; for t; do [[ $t =~ [[:space:]] ]] && a+=("'$t'") || a+=("$t"); done; echo "${a[@]}"; }
+qecho() { local a=() t; for t; do [[ $t =~ [[:space:]\'] ]] && a+=("${t@Q}") || a+=("$t"); done; echo "${a[@]}"; }
+
 
 # same as bash echo, but double-quote any args that contain whitespace
-qqecho() { local a=() t; for t; do [[ $t =~ [[:space:]] ]] && a+=("\"$t\"") || a+=("$t"); done; echo "${a[@]}"; }
+qqecho() { local a=() t; for t; do [[ $t =~ [[:space:]\"] ]] && a+=("\"${t//\"/\\\"}\"") || a+=("$t"); done; echo "${a[@]}"; }
 
 # test with command-line args
 qecho "$@"
