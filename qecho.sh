63444237f8d03f6568f2dfe8db8cacc078a0c405
glitchub <glitchub@nowhere>
2025-06-09 15:30:12 -0700

    escape either quotes

--- qecho.sh
+++ qecho.sh
@@ -1,10 +1,15 @@
-# same as bash echo, but single quote any args that contain whitespace
-qecho() { local a=() t; for t; do [[ $t =~ [[:space:]\'] ]] && a+=("${t@Q}") || a+=("$t"); done; echo "${a[@]}"; }
+# same as bash echo, but single quote any args that contain whitespace or either quote
+qecho() { local a=() t; for t; do [[ $t =~ [[:space:]\'\"] ]] && a+=("${t@Q}") || a+=("$t"); done; echo "${a[@]}"; }
 
+# same as bash echo, but double-quote any args that contain whitespace or either quote
+qqecho() { local a=() t; for t; do [[ $t =~ [[:space:]\'\"] ]] && a+=("\"${t//\"/\\\"}\"") || a+=("$t"); done; echo "${a[@]}"; }
 
-# same as bash echo, but double-quote any args that contain whitespace
-qqecho() { local a=() t; for t; do [[ $t =~ [[:space:]\"] ]] && a+=("\"${t//\"/\\\"}\"") || a+=("$t"); done; echo "${a[@]}"; }
-
-# test with command-line args
+# E.G.
+# $ bash qecho.sh This is "a test".
+# This is 'a test.'
+# This is "a test."
+# $ bash qecho.sh This isn\'t \"just\" "a test".
+# This 'isn'\''t' '"just"' 'a test.'
+# This "isn't" "\"just\"" "a test."
 qecho "$@"
 qqecho "$@"
